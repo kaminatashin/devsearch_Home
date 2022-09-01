54a6f4e2db9d38b8b21490859d418147c4ed70b2
kaminatashin <katashin@gmail.com>
2022-09-02 02:25:29 +0430

    step 2

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('project.urls')),
-    path('users/',include('users.urls')),
-
-]
+    path('users/', include('users.urls')),
+    ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
