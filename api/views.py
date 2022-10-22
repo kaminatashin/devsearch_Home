5762cc5ea00e04bd625027fcd9e3d115471ff0be
kaminatashin <katashin@gmail.com>
2022-10-22 21:09:36 +0330

    14010730

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from project.models import Project,Tag
+from project.models import Project,Tag,Review
 from users.models import Profiles
 
 from api import serializers
@@ -34,5 +34,21 @@ def getProjects(resquest):
 @api_view(['GET'])
 def getProject(request,pk):
     project=Project.objects.get(id=pk)
+    serializer=ProjectSerializer(project,many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes(IsAuthenticated)
+def projectVote(request,pk):
+    project=Project.objects.get(id=pk)
+    user=request.profiles.user
+    data=request.date
+    review,created=Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+    review.value=data['value']
+    review.save()
+    project.getVoteCount
     serializer=ProjectSerializer(project,many=False)
     return Response(serializer.data)
